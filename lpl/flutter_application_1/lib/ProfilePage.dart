77c03ca8448167b60c5fa8d8eef66514e2d6c990
omu-Dez <abdillaevmrbek@gmail.com>
2026-01-13 22:51:27 +0600

    homework-1.dart

---
+++ lpl/flutter_application_1/lib/ProfilePage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget{
+  String name = 'Jon Snow';
+  String bio = 'Game of Thrones';
+  String age = '30 years old';
+  String city = 'London, UK';
+  String interests = '🎬 Acting\n🏋️ Fitness\n🎥 Movies';
+
+@override
+  Widget build(BuildContext context) {
+return Scaffold(
+  appBar: AppBar(
+    title: Text("Profile"),
+    centerTitle: true,
+    backgroundColor: Colors.blue,
+    leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+    actions: [
+      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+      IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+    ],
+  ),
+  body:  Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CircleAvatar(
+          radius: 80,
+          backgroundImage: AssetImage('images/oioi.jpg'),
+        ),
+        SizedBox(height: 22),
+        Text(
+        name,
+        style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
+        color: Colors.black,
+        ),
+        ),
+        SizedBox(height: 5),
+            Text(
+        age,
+        style: TextStyle(
+        fontSize: 16,
+        fontFamily: 'Poppins',
+        color: Colors.black,
+        ),
+        ),
+        SizedBox(height: 5),
+        Text(
+        city,
+        style: TextStyle(
+        fontSize: 16,
+        fontFamily: 'Poppins',
+        color: Colors.black,
+        ),
+        ),
+        SizedBox(height: 5),
+        Text(
+        bio,
+        style: TextStyle(
+        fontSize: 16,
+        fontFamily: 'Poppins',
+        color: Colors.lightBlue,
+        ),
+        ),
+        SizedBox(height: 5),
+        Text( 
+        interests, 
+        textAlign: TextAlign.center,
+        style: TextStyle( 
+        fontSize: 16, 
+        fontFamily: 'Poppins', 
+        color: Colors.black, ), ),
+      ],
+    ),
+  ),
+    );
+  }
+}
---
+++ lpl/flutter_application_1/lib/profileApp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ProfilePage.dart';
+
+
+class Profileapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
